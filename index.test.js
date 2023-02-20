a3a2ca36ff4eba0d76fab4c33b0a8d37fa1091d5
DanJackson02 <dannyjjackson2002@gmail.com>
2023-02-20 09:26:43 +0000

    Added Associations between the user, board and cheese

--- index.test.js
+++ index.test.js
@@ -38,18 +38,20 @@ describe('Cheese-Board Integrity checks', () => {
         })
     })
 });
-/*------------------------------------------------------- WIP
+
 describe('Cheese-Board Assosciation checks', () => {
     beforeAll(async () => {
         await sequelize.sync({ force: true });
     });
     
-    describe("`User` and `Board` models with a One-to-Many")
+    /*
+    ("`User` and `Board` models with a One-to-Many")
     test('One `User` can have many `Board`', async () => {
         const testUser = await User.create({ name: 'Steven', email: 'Steven@failure.com' });
         const testBoard = await Board.create([{ type: 'board1', description: 'board1Description', rating: 8 },{ type: 'board2', description: 'board2Description', rating: 9 }]);
 
     });
-    WIP --------------------------------------------------------*/
-
+    */
 });
+
+
--- models/index.js
+++ models/index.js
@@ -2,4 +2,12 @@ const { Cheese } = require('./cheese');
 const { Board } = require('./board');
 const { User } = require('./User');
 
+// Multiple Boards can be added to a User.
+// A Board can have many Cheeses
+// - A Cheese can be on many Boards
+
+User.hasMany(Board)
+Board.hasMany(Cheese)
+Cheese.belongsToMany(Board)
+
 module.exports = { Cheese, Board, User }
